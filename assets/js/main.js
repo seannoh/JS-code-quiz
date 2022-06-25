3c188f843d81fecaf3b0440d8b2d4ba54f1cbe1b
Sean Oh <seanoh@ucsb.edu>
2022-06-24 18:53:10 -0700

    add js selectors for DOM elements

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+// Selectors for DOM elements
+var viewHighscoresBtn = document.getElementById("view-highscores");
+var highscoreModal = document.getElementById("highscore-modal");
+var timerEl = document.getElementById("timer");
+var startContentEl = document.getElementById("start-content");
+var startBtn = document.getElementById("start-btn");
+var quizContentEl = document.getElementById("quiz-content");
+var endContentEl = document.getElementById("end-content");
+var finalScoreEl = document.getElementById("final-score");
+var initialsForm = document.getElementById("initials-form");
+var initialsInput = document.getElementById("initials-input");
+var initialsSubmitBtn = document.getElementById("submit");
+var solutionContentEl = document.getElementById("solution-content");
+var solutionTextEl = document.getElementById("solution-text");
+
+// Variables
+
+
+
+// Functions
+
+
+
+// Function calls
